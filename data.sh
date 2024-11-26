e96e41776ec5ec73bf3a9daad20898c795f44a82
Josiah Bjorgaard <josiahbjorgaard@gmail.com>
2024-11-25 16:53:31 -0800

    adding data

--- data.sh
+++ data.sh
@@ -1,5 +1,5 @@
-mkdir -p data/hg38/
-curl https://storage.googleapis.com/basenji_barnyard2/hg38.ml.fa.gz > data/hg38/hg38.ml.fa.gz
-ungzip data/hg38/hg38.ml.fa.gz
-curl https://storage.googleapis.com/basenji_barnyard2/sequences_human.bed > data/hg38/human-sequences.bed
+mkdir -p hg38/
+curl https://storage.googleapis.com/basenji_barnyard2/hg38.ml.fa.gz > hg38/hg38.ml.fa.gz
+ungzip hg38/hg38.ml.fa.gz
+curl https://storage.googleapis.com/basenji_barnyard2/sequences_human.bed > hg38/human-sequences.bed
 
